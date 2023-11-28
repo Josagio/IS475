ec74b751bf09e56c9b2f46d1ea8e013cb7dfa573
Emily  Milan <emilan256@gmail.com>
2023-11-28 03:49:23 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -75,5 +75,5 @@ INSERT INTO CLUB(Club_ID, Purpose, Club_Name) VALUES
 );
 
 CREATE TABLE STUDENT_PHONE (
-    NSHE_ID char (10) references STUDENT(NSHE_)
+    NSHE_ID char (10) references STUDENT(NSHE_ID)
 )
