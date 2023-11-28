3aa76a5d62315bc2ff4edafcccad347a23a2f056
Emily  Milan <emilan256@gmail.com>
2023-11-28 03:41:46 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -62,7 +62,7 @@ INSERT INTO CLUB(Club_ID, Purpose, Club_Name) VALUES
 ('10038', 'Literature', 'Drama Club'),
 ('10039', 'Community Service', 'Community Builders'),
 ('10040', 'Technology', 'App Developers'),
-('1041', 'Sports', 'Track and Field'),
+('10041', 'Sports', 'Track and Field'),
 ('CLUB42', 'Science', 'Biology Buffs'),
 ('CLUB43', 'Art', 'Dance Ensemble'),
 ('CLUB44', 'Music', 'Orchestra'),
