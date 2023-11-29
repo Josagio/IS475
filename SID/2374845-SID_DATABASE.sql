6f4915054d6d78ff6f1034d110d84cdd89a353a0
Robert Leggio <leggiorobbie@gmail.com>
2023-11-29 00:46:12 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -112,6 +112,7 @@ INSERT INTO CLUB (Club_ID, Purpose, Club_Name) VALUES
 DELETE FROM CLUB
 WHERE Club_ID='10051'
 
+
 UPDATE CLUB
 SET Purpose = 'Develop and Talk about tech innovation'
 WHERE Club_ID = '10013'
