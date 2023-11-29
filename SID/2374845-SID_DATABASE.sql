eccf9d79d646bb3a77effd3ca12abe4610441906
Robert Leggio <leggiorobbie@gmail.com>
2023-11-29 00:42:31 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -109,7 +109,7 @@ INSERT INTO CLUB (Club_ID, Purpose, Club_Name) VALUES
 ('10051', 'Politics', 'Debate Society');
 
 #####Data deletion
-DELETE FROM 
+DELETE FROM CLUB
 
 Alter Table CLUB
    delete Club_Name varchar (30) NOT NULL;
