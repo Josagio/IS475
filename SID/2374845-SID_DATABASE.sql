87288e69b15d5826b5ead30443d8319a841ef11e
Connor Davis <davisc40@unlv.nevada.edu>
2023-11-29 00:41:39 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -183,7 +183,7 @@ VALUES --ChatGPT
     (666666, 'William', 'Taylor', 'Part-Time', 3.78, 'Grad'),
     (777777, 'Emma', 'Moore', 'Full-Time', 3.83, 'Undergrad'),
     (888888, 'Daniel', 'Jackson', 'Part-Time', 3.91, 'Grad'),
-    (999999, 'Ava', 'White', 'Full-Time', 3.88, 'Undergrad'),
+    (999999, 'Ava', 'White', 'Full-Time', 3.88, 'STU_UNDERGRAD'),
     (101010, 'Liam', 'Martinez', 'Part-Time', 3.96, 'Grad');
     (121212, 'Grace', 'Walker', 'Full-Time', 3.82, 'STU_UNDERGRAD'),
     (131313, 'Isaac', 'Baker', 'Part-Time', 3.75, 'Grad'),
