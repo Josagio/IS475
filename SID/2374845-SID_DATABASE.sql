4eda84f1a8737c8f3baa0bb2eb49f36a7f64b1a4
Robert Leggio <leggiorobbie@gmail.com>
2023-11-29 03:22:50 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -100,7 +100,7 @@ INSERT INTO STUDENT VALUES
 ('5566778886', 'Jackson', 'Bryant', 'P', 3.6, 'G'),
 ('1122334467', 'Liam', 'Kelly', 'F', 3.1, 'U'),
 ('5566778885', 'Lily', 'Gonzalez', 'P', 3.2, 'G')
-('5566778885', 'Lily', 'Gonzalez', 'P', 3.2, 'G');
+('200', 'Lily', 'Gonzalez', 'P', 3.2, 'G');
 
 #####Data deletion
 DELETE FROM STUDENT
