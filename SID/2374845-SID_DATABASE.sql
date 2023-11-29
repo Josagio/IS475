6bef1fed9354c159978c959e041e6c4bb46aa380
Robert Leggio <leggiorobbie@gmail.com>
2023-11-29 00:39:27 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -57,7 +57,7 @@ CREATE TABLE STUDENT_PHONE(
 
 INSERT INTO CLUB (Club_ID, Purpose, Club_Name) VALUES
 ('10001', 'Sports', 'Team A'),
-('10002', 'Fosters creativity, supports artistic expression, and builds community among those passionate about visual and creative arts.', 'Painters'),
+('10002', 'Fosters creativity, supports artistic expression, and builds community  about visual and creative arts.', 'Painters'),
 ('10003', 'Music', 'Melody Makers'),
 ('10004', 'Science', 'Tech Explorers'),
 ('10005', 'Community Service', 'Helping Hands'),
