f473d9a01998c4e57cd074092aac30db3b6ba171
Robert Leggio <leggiorobbie@gmail.com>
2023-11-29 00:35:48 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -109,7 +109,7 @@ INSERT INTO CLUB (Club_ID, Purpose, Club_Name) VALUES
 ('10051', 'Politics', 'Debate Society');
 
 INSERT INTO CLUB (Club_ID, Purpose, Club_Name) VALUES
-
+('10051', 'Politics', 'Debate Society');
 INSERT INTO STUDENT_PHONE (NSHE_ID, PHONE NUMBER) VALUES
 ('1234567890', '555-123-4567'),
 ('2345678901', '555-234-5678'),
