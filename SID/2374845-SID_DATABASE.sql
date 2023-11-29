dac28b466c87560648ff1086a379729b1c3a01af
Robert Leggio <leggiorobbie@gmail.com>
2023-11-29 03:09:25 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -125,9 +125,7 @@ INSERT INTO STU_UNDERGRAD (Minor, Internship_Exp, NSHE_ID) VALUES
 
 INSERT INTO STU_UNDERGRAD (Minor, Internship_Exp, NSHE_ID) VALUES 
 ('Computer Engineering', 'Software Innovations', '1122334455'),
-('Political Science', 'Government Affairs Office', '5566778899');
-
-
+('Political Science', 'Government Affairs Office', '5566778899')
 ('Civil Engineering', 'Construction Engineering Ltd.', '5566778885'),
 ('Economics', 'Economic Research Institute', '5544332211'),
 ('Chemistry', 'Chemical Lab Innovations', '2233445566');
