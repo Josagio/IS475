ba5a13a3eaf00cc8dee4e2787fa920ca0746eb51
Emily  Milan <emilan256@gmail.com>
2023-11-30 19:41:36 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -15,3 +15,5 @@ SELECT AVG(Budget) AS AvgBudget FROM CLUB;
 
 #Club with the highest budget 
 SELECT * FROM CLUB ORDER BY Budget DESC LIMIT 1;
+
+Retrieve the clubs with the highest budget for each purpose:
