5876c170688bf0ced6982a09480113952f656d93
Robert Leggio <leggiorobbie@gmail.com>
2023-11-30 23:52:43 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -402,45 +402,7 @@ INSERT INTO STUDENT_PHONE (NSHE_ID, PHONE_NUMBER) VALUES
 ('0011223344', '9550011223');
 
 
-INSERT INTO Students (NSHE_ID, First_Name, Last_Name, Enrollment, GPA, Student_Type)
-VALUES --ChatGPT
-    (123456, 'John', 'Doe', 'Full-Time', 3.75, 'STU_UNDERGRAD'),
-    (789012, 'Jane', 'Smith', 'Part-Time', 3.90, 'STU_GRAD'),
-    (345678, 'Bob', 'Johnson', 'Full-Time', 3.60, 'STU_UNDERGRAD'),
-    (901234, 'Alice', 'Williams', 'Part-Time', 3.85, 'STU_GRAD'),
-    (567890, 'Emily', 'Davis', 'Full-Time', 3.80, 'STU_UNDERGRAD'),
-    (123789, 'Michael', 'Brown', 'Part-Time', 3.95, 'STU_GRAD'),
-    (890123, 'Sophia', 'Miller', 'Full-Time', 3.70, 'STU_UNDERGRAD'),
-    (456789, 'David', 'Wilson', 'Part-Time', 3.88, 'STU_GRAD'),
-    (111111, 'Emily', 'Jones', 'Full-Time', 3.80, 'STU_UNDERGRAD'),
-    (222222, 'Michael', 'Davis', 'Part-Time', 3.95, 'STU_GRAD'),
-    (333333, 'Sophia', 'Brown', 'Full-Time', 3.70, 'STU_UNDERGRAD'),
-    (444444, 'David', 'Miller', 'Part-Time', 3.88, 'STU_GRAD'),
-    (555555, 'Olivia', 'Anderson', 'Full-Time', 3.92, 'STU_UNDERGRAD'),
-    (666666, 'William', 'Taylor', 'Part-Time', 3.78, 'STU_GRAD'),
-    (777777, 'Emma', 'Moore', 'Full-Time', 3.83, 'STU_UNDERGRAD'),
-    (888888, 'Daniel', 'Jackson', 'Part-Time', 3.91, 'STU_GRAD'),
-    (999999, 'Ava', 'White', 'Full-Time', 3.88, 'STU_UNDERGRAD'),
-    (101010, 'Liam', 'Martinez', 'Part-Time', 3.96, 'STU_GRAD');
-    (121212, 'Grace', 'Walker', 'Full-Time', 3.82, 'STU_UNDERGRAD'),
-    (131313, 'Isaac', 'Baker', 'Part-Time', 3.75, 'STU_GRAD'),
-    (141414, 'Chloe', 'Fisher', 'Full-Time', 3.68, 'STU_UNDERGRAD'),
-    (151515, 'Elijah', 'Cooper', 'Part-Time', 3.93, 'STU_GRAD'),
-    -- Add more data...
-    (161616, 'Avery', 'Hill', 'Full-Time', 3.79, 'STU_UNDERGRAD'),
-    (171717, 'Logan', 'Evans', 'Part-Time', 3.87, 'STU_GRAD'),
-    (181818, 'Zoe', 'Bennett', 'Full-Time', 3.94, 'STU_UNDERGRAD'),
-    (191919, 'Nathan', 'Simmons', 'Part-Time', 3.72, 'STU_GRAD'),
-    -- Add more data...
-    (202020, 'Hannah', 'Grant', 'Full-Time', 3.85, 'STU_UNDERGRAD'),
-    (212121, 'Jackson', 'Murray', 'Part-Time', 3.90, 'STU_GRAD'),
-    (222222, 'Addison', 'Gibson', 'Full-Time', 3.76, 'STU_UNDERGRAD'),
-    (232323, 'Ethan', 'Harrison', 'Part-Time', 3.98, 'STU_GRAD'),
-    -- Add more data...
-    (242424, 'Mia', 'Owens', 'Full-Time', 3.81, 'STU_UNDERGRAD'),
-    (252525, 'Lucas', 'Sharp', 'Part-Time', 3.89, 'STU_GRAD'),
-    (262626, 'Lily', 'Ford', 'Full-Time', 3.77, 'STU_UNDERGRAD'),
-    (272727, 'Carter', 'Hudson', 'Part-Time', 3.92, 'STU_GRAD');
+
 
 -- Insert sample data into STU_GRAD
 INSERT INTO STU_GRAD (NSHE_ID, Thesis_Topic, Assistant_Type)
