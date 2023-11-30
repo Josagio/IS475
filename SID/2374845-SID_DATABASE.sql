442bce44b9ca44dfd839ef6ec637be38974848f2
Emily  Milan <emilan256@gmail.com>
2023-11-30 18:42:43 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -347,6 +347,8 @@ INSERT INTO STUDENT_PHONE (NSHE_ID, PHONE NUMBER) VALUES
 ('0011223344', '555-001-1223');
 
 
+
+
 INSERT INTO Students (NSHE_ID, First_Name, Last_Name, Enrollment, GPA, Student_Type)
 VALUES --ChatGPT
     (123456, 'John', 'Doe', 'Full-Time', 3.75, 'STU_UNDERGRAD'),
