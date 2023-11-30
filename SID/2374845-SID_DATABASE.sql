ca36e0776a9a15974eb5baac67c5aae8d8b6ebc3
Emily  Milan <emilan256@gmail.com>
2023-11-30 17:11:13 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -220,7 +220,7 @@ INSERT INTO CLUB (Club_ID, Purpose, Club_Name, Budget) VALUES
 ('10051', 'Politics', 'Debate Society');
 
 UPDATE CLUB
-SET Budget = Round
+SET Budget = R
 
 #####Data deletion
 DELETE FROM CLUB
