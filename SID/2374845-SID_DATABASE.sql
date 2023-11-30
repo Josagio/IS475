6ae8071137d82530a8d34bb41cefb62b35701a7a
Emily  Milan <emilan256@gmail.com>
2023-11-30 17:32:21 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -46,7 +46,7 @@ Alter Table CLUB
 
 Alter Table CLUB 
     modify Purpose varchar (500);
-Alter 
+Alter Table Club 
 
 CREATE TABLE STUDENT_PHONE(
     NSHE_ID char(10) references STUDENT(NSHE_ID),
