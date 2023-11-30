c03001993eec3ebce2c66b06a0a992c980b69611
Emily  Milan <emilan256@gmail.com>
2023-11-30 18:12:43 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -283,6 +283,7 @@ UPDATE CLUB SET Budget = 750 WHERE Club_ID = '10044';
 UPDATE CLUB SET Budget = 5500 WHERE Club_ID = '10045';
 UPDATE CLUB SET Budget = 50000 WHERE Club_ID = '10046';
 UPDATE CLUB SET Budget = 50000 WHERE Club_ID = '10047';
+
 -- Update the Budget column to NULL for duplicate values
 UPDATE CLUB
 SET Budget = NULL
