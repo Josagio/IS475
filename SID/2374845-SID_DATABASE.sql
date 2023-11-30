0dd1c9030a6787c37239818b138391ce6ec52aca
Emily  Milan <emilan256@gmail.com>
2023-11-30 17:24:55 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -166,8 +166,7 @@ UPDATE STU_UNDERGRAD
 SET Internship_EXP = 'Corsair'
 WHERE NSHE_ID='2001699515';
 
-INSERT INTO CLUB (Club_ID, Purpose, Club_Name, Budget)
-VALUES 
+INSERT INTO CLUB (Club_ID, Purpose, Club_Name, Budget) VALUES 
 ('10001', 'Sports', 'Team A', '1000'),
 ('10002', 'Fosters creativity and artistic expression.', 'Painters', '2500'),
 ('10003', 'Music', 'Melody Makers', '1500'),
