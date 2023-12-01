abc301b35ba859aad688c8d124b69a7c9bc5e217
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 23:40:33 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -53,6 +53,7 @@ HAVING
 ---- what clubs has (313501, 'KARYN', 'BACHMEIER', 'I'), endorsed
 SELECT Club_Name 
 From CLUB
+
 Inner Join STAFF
 ON STAFF.Staff_ID = ENDORSE.Staff_ID
 Where Staff_ID = '313501';
