c465e7dde27bcdc8e3e9888736d05eafe578dd7f
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 01:49:02 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -472,7 +472,7 @@ INSERT INTO MAJOR (NSHE_ID, Major_Name) VALUES
 ('5566778885', 'Chemical Engineering'),
 ('2001699515', 'Music Composition');
 
-###Data deletion
+---Data deletion
  DELETE from MAJOR
 WHERE NSHE_ID = '222222'
 
