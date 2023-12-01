1980270e8bcc66da9e5fd0c0a13f41e9a9191b13
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 23:26:13 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -49,7 +49,7 @@ Where Student_Type='U';
 SELECT CLUB.Club_Name, COUNT(JOINS.NSHE_ID) AS StudentCount
 FROM CLUB
 LEFT Outer JOIN JOINS
-ON C.Club_ID = j.CLUB_ID
+ON CLUB.Club_ID = j.CLUB_ID
 GROUP BY
     c.Club_ID, c.Club_Name
 HAVING
