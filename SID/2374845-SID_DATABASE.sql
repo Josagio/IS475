f1b51e42fc3a683202f4db57cfdf4752a85c4b65
Connor Davis <davisc40@unlv.nevada.edu>
2023-12-01 01:01:23 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -407,6 +407,7 @@ INSERT INTO ENDORSES (Club_ID, Staff_ID) VALUES
  DELETE from ENDORSES
 WHERE NSHE_ID = '10033'
 
+
 Create TABLE MAJOR(
 NSHE_ID char (10) primary key references STUDENT(NSHE_ID),
 Major_Name char (10) not null 
