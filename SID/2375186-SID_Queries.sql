05b3259fc5fe46a58a80984f51334e5fd24fc3ed
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 23:29:37 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -34,7 +34,7 @@ SELECT Enrollment, COUNT(*) AS Student_Count FROM STUDENT GROUP BY Enrollment;
 
 
 #Let me know your thoughts
----Show
+---Showcase 
 SELECT * FROM STUDENT
 RIGHT OUTER JOIN STU_UNDERGRAD
 ON STUDENT.NSHE_ID=STU_UNDERGRAD.NSHE_ID;
