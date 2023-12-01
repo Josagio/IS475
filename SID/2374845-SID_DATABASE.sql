53f46e19c4489dfdd895b2e301ae1c538c27c242
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 20:18:20 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -580,7 +580,7 @@ INSERT INTO ENDORSES (Club_ID, Staff_ID) VALUES
 ('10036', '1077608'),
 ('10037', '10212964'),
 ('10038', '10039659'),
-('10039', '10209954'),
+('10039', '1'),
 
 --delete from endorses
  DELETE from ENDORSES
