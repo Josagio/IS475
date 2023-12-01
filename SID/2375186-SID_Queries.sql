a232b3040358d0c0e5938cef6e13cb5b4b24807e
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 23:41:05 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -54,7 +54,7 @@ HAVING
 SELECT Club_Name 
 From CLUB
 Inner Join ENDORSE
-On CLUB.Club_ID = ENDORSES.C
+On CLUB.Club_ID = ENDORSES.
 Inner Join STAFF
 ON STAFF.Staff_ID = ENDORSES.Staff_ID
 Where Staff_ID = '313501';
