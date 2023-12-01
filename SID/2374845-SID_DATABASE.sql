927bcde6964c0a39ece8299a258dcdd493983cf3
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 00:38:43 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -3,10 +3,7 @@
 ---CREATE DATABASE IF NOT EXISTS SID_Database;
 ---USE SID_Database;
 
-CREATE Table ENDORSES(
-Club_ID char(5) not null references CLUB(Club_ID),
-Staff_ID char (8) not null references STAFF(Staff_ID)
-);
+
 
 
 CREATE Table STU_GRAD(
