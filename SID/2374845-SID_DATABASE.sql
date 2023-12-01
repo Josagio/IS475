5727e69d22be586026eba3f6e2c5412c42993664
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 20:25:22 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -495,7 +495,7 @@ VALUES
     ('9876543210', 'Digital Sociology Impact on Society', 'Research Assistant');
 
 -- delete STU_GRAD
-DELETE FROM STU_FRAD
+DELETE FROM STU_GRAD
 WHERE NSHE_ID='6677889909';
 
 -- alter STU_GRAD
