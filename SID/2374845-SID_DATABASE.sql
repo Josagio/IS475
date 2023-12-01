1f20f046cd17f0ba624a067c928da7318ea77524
Connor Davis <davisc40@unlv.nevada.edu>
2023-12-01 00:25:39 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -845,7 +845,7 @@ VALUES --ChatGPT
     (393939, 'Digital Marketing Strategies', 'Teaching Assistant'),
     (404040, 'Climate Change and Global Health', 'Research Assistant'),
     (414141, 'Educational Gamification', 'Teaching Assistant');
--- update stu_grade
+-- update stu_grad table
 
     -- Insert sample data into ENDORSES
 INSERT INTO ENDORSES (Club_ID, Staff_ID)
