09c91e5a0f6bf53fd61ae79055028c10014ebfad
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 01:52:48 +0000

    Update query `SID_DATABASE`

--- SID/2374845-SID_DATABASE.sql
+++ SID/2374845-SID_DATABASE.sql
@@ -417,7 +417,7 @@ Major_Name char (10) not null
 );
 
 CREATE TABLE JOINS(
-NSHE_ID Char (10) references  STUDENT(NSHE_ID),
+    NSHE_ID Char (10) references  STUDENT(NSHE_ID),
 CLUB_ID Char (10) references  CLUB(CLUB_ID)
 );
 
