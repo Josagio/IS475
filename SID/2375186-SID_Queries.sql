a2495e66b2d2087fc4ce1bf15cf3a218cb7efc18
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 23:28:34 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -46,7 +46,7 @@ SELECT COUNT(Student_Type)
 From STUDENT
 Where Student_Type='U'; 
 
-SELECT CLUB.Club_Name AS 'Ineligble Club Name', COUNT(DISTINCT JOINS.NSHE_ID) AS '#
+SELECT CLUB.Club_Name AS 'Ineligble Club Name', COUNT(DISTINCT JOINS.NSHE_ID) AS '# of Student Members
 FROM CLUB
 LEFT Outer JOIN JOINS
 ON CLUB.Club_ID = JOINS.CLUB_ID
