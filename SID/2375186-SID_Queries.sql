35464aa1afebd14e9f95719d911684895b078c54
Robert Leggio <leggiorobbie@gmail.com>
2023-12-01 23:49:37 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -60,7 +60,7 @@ Where STAFF.Staff_ID = '313501';
 
 ---- What Club has the largest budget, and how many members do they have
 SELECT Club_Name, COUNT(DISTINCT JOINS.NSHE_ID)
-From 
+FROM C
 
 SELECT * FROM MAJOR;
 
