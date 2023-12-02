d96ca02638cfe483ebd1451d2642a67e6b818878
Robert Leggio <leggiorobbie@gmail.com>
2023-12-02 01:46:04 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -69,7 +69,7 @@ HAVING Members > (SELECT COUNT(DISTINCT J.NSHE_ID)
                     AND C.Club_Name ='Astronomy Club');
 
 ------Which major has the most students enrolled
-SELECT Major_Name, COUNT(*) AS '# of Students'
+SELECT Major_Name, COUNT(*) AS Number
 FROM MAJOR
 GROUP BY Major_Name
 ORDER BY '# of Students' DESC
