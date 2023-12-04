ff22c0a04a14834b3e0f4e0b7b5f8063982b2fb6
Emily  Milan <emilan256@gmail.com>
2023-12-04 16:26:00 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -33,7 +33,6 @@ ORDER BY CLUB.Club_Name ASC;
 SELECT * FROM STUDENT_PHONE WHERE Phone_Number LIKE '555%';
 
 #The number of students in each enrollment status:
-SELECT Enrollment, COUNT(*) AS Student_Count FROM STUDENT GROUP BY Enrollment;
 
     #or
 SELECT CLUB_ID, COUNT(NSHE_ID) AS StudentCount
