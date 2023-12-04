20a2b2a04874684baf321a4257588a68962f9d1a
Robert Leggio <leggiorobbie@gmail.com>
2023-12-04 03:38:30 +0000

    Update query `SID_Queries`

--- SID/2375186-SID_Queries.sql
+++ SID/2375186-SID_Queries.sql
@@ -26,8 +26,7 @@ From CLUB
 Inner Join ENDORSES
 On CLUB.Club_ID = ENDORSES.Club_ID
 Inner Join STAFF
-ON STAFF.Staff_ID = ENDORSES.Staff_ID
-Where STAFF.Staff_ID = '313501';
+ON STAFF.Staff_ID = ENDORSES.Staff_ID;
 
 #Select phone number with 555
 SELECT * FROM STUDENT_PHONE WHERE Phone_Number LIKE '555%';
